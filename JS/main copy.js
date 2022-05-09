4b65fed68844785c1ecb32f0d71c238ab6e2bc3d
Celeste Rodriguez <celesteerrodriguez@gmail.com>
2022-05-09 20:02:17 -0300

    modificacion de JS

---
+++ JS/main copy.js
@@ -0,0 +1,179 @@
+solicitarNombre()
+
+function solicitarNombre(){
+    let nombreIngresado   = prompt("Ingresa tu nombre")
+    console.log("Hola " + nombreIngresado + " soy tu ayudante mientras compres en nuestra tienda")
+} 
+
+// class Producto {
+//     items(id, nombre, precio) {
+//         this.id = id;
+//         this.nombre = nombre;
+//         this.precio = precio;
+//     }
+
+
+const productos =[
+    {stock: 5, id: 001, nombre: "Camiseta titular 2022", precio: 5800},
+    {stock: 7, id: 002, nombre: "Camiseta suplente 2022", precio: 5800},
+    {stock: 9, id: 003, nombre: "Conjunto de entrenamiento 2022", precio: 9800},
+    {stock: 7, id: 004, nombre: "Short futbol azul 2022", precio: 2250},
+    {stock: 8, id: 005, nombre: "Short futbol blanco 2022", precio: 2250},
+    {stock: 7, id: 006, nombre: "Remera entrenamiento azul 2022", precio: 4590},
+    {stock: 6, id: 007, nombre: "Remera entrenamiento blanca 2022", precio: 4590}
+]
+
+
+comprar();
+
+function comprar() {
+    { 
+        console.log("1: Camiseta titular 2022")
+        console.log("2: Camiseta suplente 2022")
+        console.log("3: Conjunto de entrenamiento 2022")
+        console.log("4:Short futbol azul 2022")
+        console.log("5: Short futbol blanco 2022")
+        console.log("6: Remera entrenamiento azul 2022")
+        console.log("7: Remera entrenamiento blanca 2022")
+        
+    
+        let seleccion = prompt("Seleccione su producto favorito")
+        switch(seleccion){
+            case "1":
+                let id001 = console.log("Se agregará el producto al carrito")
+                
+                agregarProducto()
+                break;
+    
+            case "2":
+                let id002 = console.log("Se agregó el producto al carrito")
+                agregarProducto()
+                break;
+            case "3":
+                let id003 = console.log("Se agregó el producto al carrito")
+                agregarProducto()
+                break;
+            case "4":
+                let id004 = console.log("Se agregó el producto al carrito")
+                agregarProducto()
+                break;
+            case "5":
+                let id005 = console.log("Se agregó el producto al carrito")
+                agregarProducto()
+                break;
+            case "6":
+                let id006 = console.log("Se agregó el producto al carrito")
+                agregarProducto()
+                break;
+            case "7":
+                let id007 = console.log("Se agregó el producto al carrito")
+                agregarProducto()
+                break;
+                
+            default:
+                    alert("Operación invalida")
+                    break;
+        }
+    }
+}
+
+
+
+function agregarProducto(p){
+        continuarCompra =confirm("¿Desea contiuar comprando?")
+        if (continuarCompra) {let continuar = console.log("Ya podes seguir agregando productos a tu carrito")
+        comprar()
+        }else {
+        finalizarCompra =confirm("¿Deseas pagar lo que tenes en tu carrito?")
+        alert("Aguarde, está siendo derivado al carrito de compras")
+        if(finalizarCompra){pagar()} else {
+            alert("Proceso de compra cancelado")
+            }
+        }
+    }
+
+
+
+ function pagar() { 
+      let total = 0
+      let seleccion = productos.filter(item => item.nombre + item.precio)
+      console.log(seleccion)
+      let carrito = productos.reduce((total, item)=> total += item.precio, 0)
+      console.log(carrito)
+    }
+//      productos.forEach((seleccion) =>{
+//         if(seleccion === 1 || 2 || 3 || 4 || 5 || 6 || 7){
+//             return total += productos.id + " " + productos.nombre + " " + productos.precio
+//         }else{alert("No hay producto en stock")}
+// })
+// }    
+    
+
+function seleccionarPago (){
+    console.log("1: Efectivo")
+    console.log("2: Tarjeta")
+    console.log("3: Tranferencia bancaria")
+    
+
+    let seleccion = prompt("Seleccione un método de pago")
+    switch(seleccion){
+        case "1":
+            let efectivo = alert("Pagarás tu producto en efectivo cuando lo recibas")
+            finCompra()
+            break;
+
+        case "2":
+            seleccionarTarjeta()
+            break;
+        
+        case "3":
+            let tranferencia = alert("Se ha enviado a su mail los datos para el deposito. Una vez confirmado el pago, el producto se enviará a la dirección ingresada")
+            finCompra()
+            break;
+
+        default:
+            alert("Operación invalida")
+            break;
+    }
+}
+
+function seleccionarTarjeta(t){
+
+    console.log("1: Visa")
+    console.log("2: Mastercard")
+    console.log("3: Américan Express")
+    console.log("3: Cabal")
+    
+
+    let seleccionT = prompt("Seleccione su tarjeta")
+    switch(seleccionT){
+        case "1":
+            let visa = alert("Pagarás tu producto con Visa")
+            finCompra()
+            break;
+        case "2":
+            let master = alert("Pagarás tu producto con Mastercard")
+            finCompra()
+            break;
+        case "3":
+            let american = alert("Pagarás tu producto con American Express")
+            finCompra()
+            break;
+        case "4":
+            let cabal = alert("Pagarás tu producto con Cabal")
+            finCompra()
+            break;
+        default:
+            alert("Operación invalida")
+            break;
+    }
+}
+    
+
+    function finCompra(){
+        alert("Gracias por confiar en nosotros, que disfrutes tu compra")
+        
+    }
+
+    //Deberias tener un array de objetos. Y trabajar con un forEach, Dentro de tu array de objetos deberías tener toda la información. Codigo, descripción, precio, etc
+
--- JS/main.js
+++ JS/main.js
@@ -1,142 +1,296 @@
-solicitarNombre()
+// solicitarNombre()
 
-function solicitarNombre(){
-    let nombreIngresado   = prompt("Ingresa tu nombre")
-    alert("Hola " + nombreIngresado + " soy tu ayudante mientras compres en nuestra tienda")
-} 
+// function solicitarNombre(){
+//     let nombreIngresado   = prompt("Ingresa tu nombre")
+//     console.log("Hola " + nombreIngresado + " soy tu ayudante mientras compres en nuestra tienda")
+// } 
 
-function Producto(nombre, precio, disponible) {
-    this.nombre ="nombre";
-    this.precio ="precio";
-    this.disponible ="disponible";
-}
-
-let p1 = new Producto("Camiseta titular 2022, 5800, true")
-let p2 = new Producto("Camiseta suplente 2022, 5800, true")
-let p3 = new Producto("Conjunto de entrenamiento 2022, 9800, true")
+// // class Producto {
+// //     items(id, nombre, precio) {
+// //         this.id = id;
+// //         this.nombre = nombre;
+// //         this.precio = precio;
+// //     }
 
 
-comprar();
+// const productos =[
+//     {stock: 5, id: 001, nombre: "Camiseta titular 2022", precio: 5800},
+//     {stock: 7, id: 002, nombre: "Camiseta suplente 2022", precio: 5800},
+//     {stock: 9, id: 003, nombre: "Conjunto de entrenamiento 2022", precio: 9800},
+//     {stock: 7, id: 004, nombre: "Short futbol azul 2022", precio: 2250},
+//     {stock: 8, id: 005, nombre: "Short futbol blanco 2022", precio: 2250},
+//     {stock: 7, id: 006, nombre: "Remera entrenamiento azul 2022", precio: 4590},
+//     {stock: 6, id: 007, nombre: "Remera entrenamiento blanca 2022", precio: 4590}
+// ]
 
-function comprar() {
-    { 
-        console.log("1: Camiseta titular 2022")
-        console.log("2: Camiseta suplente 2022")
-        console.log("3: Conjunto de entrenamiento 2022")
+
+// comprar();
+
+// function comprar() {
+//     { 
+//         console.log("1: Camiseta titular 2022")
+//         console.log("2: Camiseta suplente 2022")
+//         console.log("3: Conjunto de entrenamiento 2022")
+//         console.log("4:Short futbol azul 2022")
+//         console.log("5: Short futbol blanco 2022")
+//         console.log("6: Remera entrenamiento azul 2022")
+//         console.log("7: Remera entrenamiento blanca 2022")
         
     
-        let seleccion = prompt("Seleccione su producto favorito")
-        switch(seleccion){
-            case "1":
-                let titular2022 = alert("Se agregará el producto al carrito")
+//         let seleccion = prompt("Seleccione su producto favorito")
+//         let carrito = []
+
+//         switch(seleccion){
+//             case "1":
+//                 console.log("Se agregará el producto al carrito")
                 
-                agregarProducto()
-                break;
-    
-            case "2":
-                let suplente2022 = alert("Se agregó el producto al carrito")
-                agregarProducto()
-                break;
-            case "3":
-                let conjunto2022 = alert("Se agregó el producto al carrito")
-                agregarProducto()
-                break;
+//                 if(seleccion==1) {  
+//                     carrito.push ([0]) 
+//                 }
+//                 agregarProducto()
+//                 break;
                 
-            default:
-                    alert("Operación invalida")
-                    break;
-        }
-    }
-}
-
-function agregarProducto(p) {
-
-        continuarCompra =confirm("¿Desea contiuar comprando?")
-        if (continuarCompra) {let continuar = alert("Ya podes seguir agregando productos a tu carrito")
-        comprar()
-        }else {
-        finalizarCompra =confirm("¿Deseas pagar lo que tenes en tu carrito?") 
-        if(finalizarCompra){pagar()} else {
-            alert("Proceso de compra cancelado")
-            }
-}     
-
-
-pagar()
-
-function pagar(){
-    let finalizarCompra =alert("Aguarde, está siendo derivado al carrito de compras")
-    seleccionarPago()
-    // irCarrito()
-}
-
-function irCarrito(){
-    //Acá quisisera que aparezca el nombre de los productos seleccionados con el precio + la suma total antes de ir seleccionar el metodo de pago
-
     
-}
-function seleccionarPago (){
-    console.log("1: Efectivo")
-    console.log("2: Tarjeta")
-    console.log("3: Tranferencia bancaria")
+//             case "2":
+//                 console.log("Se agregará el producto al carrito")
+//                 if(seleccion==2) { 
+//                     carrito.push ([1]) 
+//                 }
+//                 agregarProducto()
+//                 break;
+//             case "3":
+//                 console.log("Se agregará el producto al carrito")
+//                 if(seleccion==3) {  
+//                     carrito.push ([2]) 
+//                 }
+//                 agregarProducto()
+//                 break;
+//             case "4":
+//                 console.log("Se agregará el producto al carrito")
+//                 if(seleccion==4) {  
+//                     carrito.push ([3]) 
+//                 }
+//                 agregarProducto()
+//                 break;
+//             case "5":
+//                 console.log("Se agregará el producto al carrito")
+//                 if(seleccion==5) {  
+//                     carrito.push ([4]) 
+//                 }
+//                 agregarProducto()
+//                 break;
+//             case "6":
+//                 console.log("Se agregará el producto al carrito")
+//                 if(seleccion==6) {  
+//                     carrito.push ([5]) 
+//                 }
+//                 agregarProducto()
+//                 break;
+//             case "7":
+//                 console.log("Se agregará el producto al carrito")
+//                 if(seleccion==7) {  
+//                     carrito.push ([6]) 
+//                 }
+//                 agregarProducto()
+//                 break;
+                
+//             default:
+//                     alert("Operación invalida")
+//                     break;
+//         }
+//     }
+// }
+
+
+
+// function agregarProducto(p){
+//         continuarCompra =confirm("¿Desea contiuar comprando?")
+//         if (continuarCompra) {let continuar = console.log("Ya podes seguir agregando productos a tu carrito")
+//         comprar()
+//         }else {
+//         finalizarCompra =confirm("¿Deseas pagar lo que tenes en tu carrito?")
+//         alert("Aguarde, está siendo derivado al carrito de compras")
+//         if(finalizarCompra){
+//             pagar() } else {
+//             alert("Proceso de compra cancelado")
+//             }
+//         }
+// }
+
+// //TODAS MIS DUDAS VIENEN EN COMO LINKEAR LO QUE SE SELECCIONA EN COMPRAR() PARA LOGRAR PAGARLO ACA
+
+//  function pagar() {
+//      let total = 0
+//      return (carrito) => {
+//         total += productos.id + " " + productos.nombre + " " + productos.precio
+//      }
+//       console.log(carrito)
+//  }
     
 
-    let seleccion = prompt("Seleccione un método de pago")
-    switch(seleccion){
-        case "1":
-            let efectivo = alert("Pagarás tu producto en efectivo cuando lo recibas")
-            finCompra()
-            break;
+// function seleccionarPago (){
+//     console.log("1: Efectivo")
+//     console.log("2: Tarjeta")
+//     console.log("3: Tranferencia bancaria")
+    
 
-        case "2":
-            seleccionarTarjeta()
-            break;
+//     let seleccion = prompt("Seleccione un método de pago")
+//     switch(seleccion){
+//         case "1":
+//             let efectivo = alert("Pagarás tu producto en efectivo cuando lo recibas")
+//             finCompra()
+//             break;
+
+//         case "2":
+//             seleccionarTarjeta()
+//             break;
         
-        case "3":
-            let tranferencia = alert("Se ha enviado a su mail los datos para el deposito. Una vez confirmado el pago, el producto se enviará a la dirección ingresada")
-            finCompra()
-            break;
+//         case "3":
+//             let tranferencia = alert("Se ha enviado a su mail los datos para el deposito. Una vez confirmado el pago, el producto se enviará a la dirección ingresada")
+//             finCompra()
+//             break;
 
-        default:
-            alert("Operación invalida")
-            break;
+//         default:
+//             alert("Operación invalida")
+//             break;
+//     }
+// }
+
+// function seleccionarTarjeta(t){
+
+//     console.log("1: Visa")
+//     console.log("2: Mastercard")
+//     console.log("3: Américan Express")
+//     console.log("3: Cabal")
+    
+
+//     let seleccionT = prompt("Seleccione su tarjeta")
+//     switch(seleccionT){
+//         case "1":
+//             let visa = alert("Pagarás tu producto con Visa")
+//             finCompra()
+//             break;
+//         case "2":
+//             let master = alert("Pagarás tu producto con Mastercard")
+//             finCompra()
+//             break;
+//         case "3":
+//             let american = alert("Pagarás tu producto con American Express")
+//             finCompra()
+//             break;
+//         case "4":
+//             let cabal = alert("Pagarás tu producto con Cabal")
+//             finCompra()
+//             break;
+//         default:
+//             alert("Operación invalida")
+//             break;
+//     }
+// }
+    
+
+//     function finCompra(){
+//         alert("Gracias por confiar en nosotros, que disfrutes tu compra")
+        
+//     }
+
+
+const nov = [
+    {
+    stock: 5, 
+    id: 001, 
+    nombre: 'Camiseta titular 2022', 
+    precio: 5800,
+    img:'../img/camiseta-titular-2022.png'
+},
+    {
+    stock: 9, 
+    id:002, 
+    nombre: 'Camiseta suplente 2022', 
+    precio: 5800,
+    img:'../img/camiseta-alternativa-2022.png'
+},
+    {
+    stock: 7, 
+    id:003, 
+    nombre: 'Camiseta arquero titular 2022', 
+    precio: 5800,
+    img:'../img/camiseta-arquero-titular-2022.png'
+},
+
+]
+
+const contenedorNovedades = document.querySelector(".contenedor-novedades");
+const contenedorCarrito = document.querySelector(".contenedor-carrito");
+const carrito = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarNovedades();
+})
+
+function mostrarNovedades(){
+    for (const novedades of nov){
+
+        const divNovedades = document.createElement('div');
+        divNovedades.classList.add('card');
+
+        const imgNovedades = document.createElement('img');
+        imgNovedades.classList.add('img-novedades');
+        imgNovedades.src = novedades.img;
+
+        const tituloNovedades = document.createElement('h2');
+        tituloNovedades.classList.add('titulo-novedades');
+        tituloNovedades.textContent = novedades.nombre;
+
+        const btnNovedades = document.createElement('button');
+        btnNovedades.classList.add('btn-alcarrito');
+        btnNovedades.textContent = "Añadir al carrito";
+        btnNovedades.onclick = () => {
+            agregarAlCarrito(novedades.nombre)
+        };
+
+        divNovedades.appendChild(imgNovedades)
+        divNovedades.appendChild(tituloNovedades)
+        divNovedades.appendChild(btnNovedades)
+
+        contenedorNovedades.appendChild(divNovedades)
+
     }
 }
 
-function seleccionarTarjeta(t){
+function agregarAlCarrito(name) {
 
-    console.log("1: Visa")
-    console.log("2: Mastercard")
-    console.log("3: Américan Express")
-    console.log("3: Cabal")
-    
+    const productoAgregado = nov.find( novedades => novedades.nombre === name);
+    carrito.push(productoAgregado);
+    mostrarCarrito(carrito);
+}
 
-    let seleccionT = prompt("Seleccione su tarjeta")
-    switch(seleccionT){
-        case "1":
-            let visa = alert("Pagarás tu producto con Visa")
-            finCompra()
-            break;
-        case "2":
-            let master = alert("Pagarás tu producto con Mastercard")
-            finCompra()
-            break;
-        case "3":
-            let american = alert("Pagarás tu producto con American Express")
-            finCompra()
-            break;
-        case "4":
-            let cabal = alert("Pagarás tu producto con Cabal")
-            finCompra()
-            break;
-        default:
-            alert("Operación invalida")
-            break;
-    }
-        }
-    }
+function mostrarCarrito(mCarrito){
+    mCarrito.forEach(mostrarC => {
+        const divNovedades = document.createElement('div');
+        divNovedades.classList.add('card');
 
-    function finCompra(){
-        alert("Gracias por confiar en nosotros, que disfrutes tu compra")
+        const div2Novedades = document.createElement('div');
+        divNovedades.classList.add('carrito');
+
+        const tituloNovedades = document.createElement('h4');
+        tituloNovedades.classList.add('titulo-novedades');
+        tituloNovedades.textContent = mostrarC.nombre;
+
+        const precioNovedades = document.createElement('h4');
+        precioNovedades.classList.add('precio-novedades');
+        precioNovedades.textContent = mostrarC.precio;
+
+        divNovedades.appendChild(precioNovedades)
+        divNovedades.appendChild(tituloNovedades)
+        divNovedades.appendChild(div2Novedades)
+        
+
+        contenedorCarrito.appendChild(divNovedades)
+
+    });
+    for (const novedades of nov){
+
         
     }
+}
